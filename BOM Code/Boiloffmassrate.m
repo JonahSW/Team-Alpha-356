90965cba5b0776ba4a77762f98d12d665ef38058
michaelpedota <78390766+michaelpedota@users.noreply.github.com>
2021-03-28 18:37:37 -0400

    Delete BOM Code directory

--- BOM Code/Boiloffmassrate.m
+++
@@ -1,7 +0,0 @@
-function [R_S_P, BOM_Rate] = Boiloffmassrate(BOM_important, time_important, R_s_p)
-
-a_u = 1.5E+8; % AU to Km
-R_S_P = (a_u*1000)*(R_s_p((30:length(R_s_p))));
-BOM_Rate= real(BOM_important./time_important);
-
-end 
