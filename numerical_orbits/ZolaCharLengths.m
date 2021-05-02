6fe8fa59177ccb725668d82bb1b381452f7805ee
samfaul135 <samfaulk@sams-mbp.lan>
2021-05-02 17:22:49 -0400

    more additions to ZolaCharLengths.m

--- numerical_orbits/ZolaCharLengths.m
+++ numerical_orbits/ZolaCharLengths.m
@@ -1,62 +1,61 @@
 %% Zola Characteristic Lengths
 % 5.2.21
 % sdf33
-clear
-clc
-close all
+clear; clc; close all
 
 %...Project Demeter
 %...low-thrust trajectory from Earth to Ceres, and return
 %...constant thrust
 %...note: t_ , time, values start from t = 0
 
-%...General Parameters
+%...general parameters
 g = 9.807; % m/s2, Earth gravity
 
-%...Alpha Parameters
+%...Alpha parameters
 mo = 325000; % kg, initial wet mass
 mp = 300000; % kg, propellant mass
 F = 102.1; % N, thrust
+ao = F/mo; % m/s2, initial acceleration
 Isp = 8180; % sec, specific impulse
 vj = Isp*g; % effective velocity
 
 
-%%...Phase 1...accelerating
+%%...phase 1...accelerating, 0 < t < t1
 t1 = 100; % days, time
 t1 = t1*24*3600; % secs, time
 tphase1 = 0:1:t1;
-a1_hist = ao / (1 - ao*tphase1/vj); % m/s2, acceleration history as a function of t, for 0 < t < t1
-a1 = ao / (1 - ao*t1/vj); % m/s2, acceleration magnitude, for 0 < t < t1
-V1_hist = -vj*ln(1-ao*tphase1/vj); % m/s, velocity history as a function of t, for 0 < t < t1
-V1 = -vj*ln(1-ao*t1/vj); % m/s, velocity magnitude, for 0 < t < t1
-L1 = (vj^2/ao)*((1-ao*t1/vj)*ln(1-ao*t1/vj) + ao*t1/vj); % m, characteristic length of Phase 1
+a1_hist = ao./(1 - ao.*tphase1./vj); % m/s2, acceleration history as a function of t
+a1 = ao./(1 - ao.*t1./vj); % m/s2, acceleration magnitude
+V1_hist = -vj.*log(1-ao.*tphase1./vj); % m/s, velocity history as a function of t
+V1 = -vj.*log(1-ao.*t1./vj); % m/s, velocity magnitude
+L1 = (vj.^2./ao).*((1-ao.*t1./vj).*log(1-ao.*t1./vj) + ao.*t1./vj); % m, characteristic length
 
-%%...Phase 2...coasting
-t2 = 400; % days, time
+%%...phase 2...coasting, t1 < t < t2
+t2 = t1+200; % days, time
 t2 = t2*24*3600; % secs, time
-tphase2 = t1:1:t1+t2;
-Vmax = V1; % m/s, maximum velocity during coasting, for t1 < t < t2
-L2 = Vmax*(t2-t1); % m, characteristic length, for t1 < t < t2
+tphase2 = t1:1:t2;
+Vmax = V1; % m/s, maximum velocity during coasting
+L2 = Vmax.*(t2-t1); % m, characteristic length
 
-%%...Phase 3...deccelerating
-T = 100; % days, time
+%%...phase 3...deccelerating, t2 < t < T
+T = t2+100; % days, time
 T = T*24*3600; % secs, time
 tphase3 = t2:1:T;
-a3_hist = ao/(1 - ao*t1/vj - ao*(t-t2)/vj); % m/s2, accleration history as a function of t, for t2 < t < T
-V3_hist = -vj*ln(1-ao*tphase3/vj); % m/s, velocity history as a function of t, for 0 < t < t1
-L3 = vj^2/ao*((ai*t1/vj)^2 - (1-ao*t1/vj)*ln(1-ao*t1/vj) - ao*t1/vj); % m, characteristic length, for t2 < t < T
+a3_hist = ao./(1-ao.*t1./vj-ao.*(tphase3-t2)./vj); % m/s2, accleration history as a function of t
+V3_hist = -vj.*log(1-ao.*tphase3./vj); % m/s, velocity history as a function of t
+L3 = vj.^2./ao.*((ao.*t1./vj).^2-(1-ao.*t1./vj).*log(1-ao.*t1./vj) - ao.*t1./vj); % m, characteristic length
 
-%%...Mission Totals
+%%...mission totals
 L = L1+L2+L3;
 
-
+%%...plotting
 figure
 hold on
 xlabel('Time (sec)')
 ylabel('Velocity (m/s)')
 plot(tphase1,V1_hist)
 plot(tphase2,Vmax)
-plot(tphase3,)
+plot(tphase3,V3_hist)
 
 
 
