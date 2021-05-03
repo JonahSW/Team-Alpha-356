bdce669b8fe1a9b8da844a5883e0121c7159882e
JonahSW <jsachsw@gmail.com>
2021-05-03 12:59:38 -0400

    Updated and added lunar rendezvous to alpha_traj_wrapper

--- numerical_orbits/patched_low_thrust/Project_Demeter/alpha_traj_wrapper.m
+++ numerical_orbits/patched_low_thrust/Project_Demeter/alpha_traj_wrapper.m
@@ -8,29 +8,32 @@ clc
 %% Constants
 
 %Alpha Spacecraft
-LEO_assembly_altitude = 800e3;% [m]
-LEO_capture_altitude = 1000e3;% [m]
-LCO_altitude = 700e3;%[m]
-launch_inclination = 28.573*(pi/180);
 Isp = 8426;% [s]
 Isp_kick = 302.9;% [s]
-g0 = 9.807;% [m/s^2]
-m_dry = 300e3;% [kg]
+m_dot_propellant = 1.51e-3;%[kg/s]
+ep_thrust = 122.32;% [N]
+m_dry = 317e3;% [kg]
 m_dry_kickstage = 531.2;% [kg]
 m_wet_kickstage = 5031.84;% [kg]
 m_payload = 14636;% [kg]
 m_sample = 50;% [kg]
-m_dot_propellant = 1.51e-3;%[kg/s]
-ceres_loiter_time = 60*(24*3600);% [s]
-deltaV_kickstage = 100;% [m/s]
-ep_thrust = 122.32;% [N]
 num_boosters = 4;% [kg]
+
+LEO_departure_dry_mass = 486860;% [kg] (UPDATE VALUES AS CALCS ITERATE)
+
+LEO_assembly_altitude = 800e3;% [m]
+LEO_capture_altitude = 100e3;% [m]
+LLO_capture_altitude = 10000e3;% [m]
+LCO_altitude = 700e3;%[m]
+launch_inclination = 28.573*(pi/180);
+ceres_loiter_time = 60*(24*3600);% [s]
 
 %Solar System
 %Sun
 mu_sun = 1.32712440018e20; %[m^3/s^2]
 AU = 149597870.7e3;% [m]
 %Earth
+g0 = 9.807;% [m/s^2]
 mu_earth = 3.986004418e14; %[m^3/s^2]
 earth_ephemeris = read_ephemeris(2);
 a_earth = mean(earth_ephemeris(3,:))*AU;
@@ -45,35 +48,36 @@ r_ceres_SOI = 7709455.15;% [m]
 ceres_ephemeris = read_ephemeris(4);
 a_ceres = mean(ceres_ephemeris(3,:))*AU;
 in_ceres = mean(ceres_ephemeris(4,:));
+%Moon
+mu_moon = 4904869500000;% [m^3/s^2]
+r_moon = 1737400;% [m]
+r_moon_SOI = 66100000;% [m]
+a_moon = 384399000;% [m]
+in_moon = 5.145;% [deg]
 
 %% Kick Stage Sizing (Partial kick)
 %Chemical Kick Stage for LEO Departure
 solid_propellant_mass = num_boosters*(m_wet_kickstage-m_dry_kickstage);% [kg]
-LEO_departure_dry_mass = 620e3;% [kg] (UPDATE VALUES AS CALCS ITERATE)
 deltaV_kick = Isp_kick*g0*log((LEO_departure_dry_mass+solid_propellant_mass)/LEO_departure_dry_mass);
 disp(['Delta V for the kick stage is: ',num2str(deltaV_kick),' m/s']);
 
 %% Estimate Delta Vs
-%Assume a low thrust gradual spiral with a sub-optimal plane change
+%Assume a low thrust gradual spiral with a sub-optimal climb and plane change
 
 %DeltaV for leaving Earth's SOI (1)
 r0 = LEO_assembly_altitude + r_earth;
 r = r_earth_SOI;
-v1 = sqrt(mu_earth/r0);
-v2 = sqrt(mu_earth/r);
 delta_i = (launch_inclination - earth_axial_tilt)*180/pi;% plane change for leaving Earth, deg
-deltaV1 = spiral(mu_earth,r0,r) + inclination_change(v1,v2,delta_i);
+deltaV1 = spiral_with_inclination_change(mu_earth,r0,r,delta_i) - deltaV_kick;
 %REPLACE WITH OUTBOUND CALC FROM LOW_THRUST_MODIFIED and ALPHA_EARTH_OUT.in
-deltaV1 = 7122.479;% [m/s]
-V1_excess = 809;% [m/s]
+%deltaV1 = 7122.479;% [m/s]
+%V1_excess = 809;% [m/s]
 
 %DeltaV for traveling to Ceres (2)
 r0 = a_earth;
 r = a_ceres;
-v1 = sqrt(mu_sun/r0);
-v2 = sqrt(mu_sun/r);
-delta_i = (in_earth + in_ceres)*180/pi;% plane around sun, deg
-deltaV2 = spiral(mu_sun,r0,r) + inclination_change(v1,v2,delta_i) - V1_excess;
+delta_i = (in_earth + in_ceres)*180/pi;% plane change around sun, deg
+deltaV2 = spiral_with_inclination_change(mu_sun,r0,r,delta_i);% - V1_excess;
 
 %DeltaV for capturing in Ceres SOI (3)
 r0 = r_ceres_SOI;
@@ -85,28 +89,37 @@ r0 = LCO_altitude + r_ceres;
 r = r_ceres_SOI;
 deltaV4 = spiral(mu_ceres,r0,r);
 %REPLACE WITH OUTBOUND CALC FROM LOW_THRUST_MODIFIED and ALPHA_CERES_OUT.in
-deltaV4 = 358.257;% [m/s]
-V4_excess = 179;% [m/s]
+%deltaV4 = 358.257;% [m/s]
+%V4_excess = 179;% [m/s]
 
 %DeltaV for traveling to Earth (5)
 r0 = a_ceres;
 r = a_earth;
-v1 = sqrt(mu_sun/r0);
-v2 = sqrt(mu_sun/r);
-delta_i = (in_earth + in_ceres)*180/pi;% plane around sun, deg
-deltaV5 = spiral(mu_sun,r0,r) + inclination_change(v1,v2,delta_i) - V4_excess;
+delta_i = (in_earth + in_ceres)*180/pi;% plane change around sun, deg
+deltaV5 = spiral_with_inclination_change(mu_sun,r0,r,delta_i);% - V4_excess;
 
 %DeltaV for capturing in Earth SOI (6)
 r0 = r_earth_SOI;
-r = LEO_capture_altitude + r_earth;
+r = a_moon;
 deltaV6 = spiral(mu_earth,r0,r);
+
+%DeltaV for capturing in Lunar SOI (7)
+r0 = a_moon;
+r = LLO_capture_altitude;
+delta_i = in_moon*pi/180;%Plane change to lunar orbit inclination, deg
+deltaV7 =   spiral_with_inclination_change(mu_moon,r0,r,delta_i);
 
 %% Mission Phases
 m_final = m_dry+m_sample-m_payload;% [kg]
+%Moon Inbound
+mratio_7 = exp(deltaV7/(Isp*g0));
+m_wet_7 = m_final*mratio_7;% [kg]
+m_propellant_7 = m_wet_7 - m_final;% [kg]
+duration_7 = m_propellant_7/m_dot_propellant;% [s]
 %Earth Inbound
 mratio_6 = exp(deltaV6/(Isp*g0));
-m_wet_6 = m_final*mratio_6;% [kg]
-m_propellant_6 = m_wet_6 - m_final;% [kg]
+m_wet_6 = m_wet_7*mratio_6;% [kg]
+m_propellant_6 = m_wet_6 - m_wet_7;% [kg]
 duration_6 = m_propellant_6/m_dot_propellant;% [s]
 %Sun Inbound
 mratio_5 = exp(deltaV5/(Isp*g0));
@@ -140,11 +153,11 @@ duration_1 = m_propellant_1/m_dot_propellant;% [s]
 m_wet_initial = m_wet_1 + m_wet_kickstage;% [kg]
 %mratio1 = exp(deltaV1/(Isp*g0))
 %% Results
-deltaV_total = deltaV1+deltaV2+deltaV3+deltaV4+deltaV5+deltaV6;% [m/s]
-mratio_total = mratio_1*mratio_2*mratio_3*mratio_4*mratio_5*mratio_6;
-m_propellant_total = (m_propellant_1+m_propellant_2+m_propellant_3+m_propellant_4+m_propellant_5+m_propellant_6);% [kg]
-duration_thrust = (duration_1+duration_2+duration_3+duration_4+duration_5+duration_6)/(24*3600);% [days] 
-duration_total = (duration_1+duration_2+duration_3+duration_ceres+duration_4+duration_5+duration_6)/(24*3600);% [days]
+deltaV_total = deltaV1+deltaV2+deltaV3+deltaV4+deltaV5+deltaV6+deltaV7;% [m/s]
+mratio_total = mratio_1*mratio_2*mratio_3*mratio_4*mratio_5*mratio_6*mratio_7;
+m_propellant_total = (m_propellant_1+m_propellant_2+m_propellant_3+m_propellant_4+m_propellant_5+m_propellant_6+m_propellant_7);% [kg]
+duration_thrust = (duration_1+duration_2+duration_3+duration_4+duration_5+duration_6+duration_7)/(24*3600);% [days] 
+duration_total = (duration_1+duration_2+duration_3+duration_ceres+duration_4+duration_5+duration_6+duration_7)/(24*3600);% [days]
 disp(['The total mission deltaV is ',num2str(deltaV_total*1e-3),' km/s.']);
 disp(['The total mission mass ratio is ',num2str(mratio_total),'']);
 disp(['The total spacecraft wet mass is ',num2str(m_wet_1),' kg.']);
@@ -153,8 +166,8 @@ disp(['The total thrusting duration is ',num2str(duration_thrust),' days.']);
 disp(['The total mission duration is ',num2str(duration_total),' days.']);
 
 figure()
-durations = [0,duration_1,duration_2,duration_3,duration_ceres,duration_4,duration_5,duration_6];
-masses = [m_wet_initial,m_wet_1,m_wet_2,m_wet_3,m_wet_ceres,m_wet_4,m_wet_5,m_wet_6,m_dry];
+durations = [0,duration_1,duration_2,duration_3,duration_ceres,duration_4,duration_5,duration_6,duration_7];
+masses = [m_wet_initial,m_wet_1,m_wet_2,m_wet_3,m_wet_ceres,m_wet_4,m_wet_5,m_wet_6,m_wet_7,m_final];
 plot(1:1:length(masses),masses,'o');
 grid minor
 xline(1,'-','LEO Assembly Mass');
@@ -165,7 +178,8 @@ xline(5,'-','After Ceres Spiral In');
 xline(6,'-','At Ceres Departure');
 xline(7,'-','After Ceres Spiral Out');
 xline(8,'-','After Sun Spiral In');
-xline(9,'-','After LEO Spiral In','LabelHorizontalAlignment','left');
+xline(9,'-','After Earth Spiral In');
+xline(10,'-','After Moon Spiral In','LabelHorizontalAlignment','left');
 title('Mass over Mission Duration');
 ylabel('Mass (kg)');
 xlabel('Time');
@@ -178,7 +192,9 @@ function deltaV = spiral(mu,r0,r)
 end
 
 %Calculates the deltaV for a sub optimal low thrust plane change given the initial and final velocities
-function deltaV = inclination_change(v1,v2,delta_i)
+function deltaV = spiral_with_inclination_change(mu,r0,r,delta_i)
+    v1 = sqrt(mu/r0);
+    v2 = sqrt(mu/r);
     deltaV = sqrt(v1^2 + v2^2 - 2*v1*v2*cos((delta_i*pi/180)*pi/2));
 end
 
